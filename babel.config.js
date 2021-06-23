35efe376f488afb392c41bb590aeada12b5b9816
Kaken Bok <bok@kakenbok.de>
2021-06-23 18:01:04 +0200

    add 'loose: true' to babel config

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = {
   presets: ['@vue/babel-preset-app'],
   plugins: [
     ['@babel/plugin-proposal-decorators', { legacy: true }],
-    ['@babel/plugin-proposal-class-properties', { loose: true }]
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-private-methods', { loose: true }]
   ]
 }
